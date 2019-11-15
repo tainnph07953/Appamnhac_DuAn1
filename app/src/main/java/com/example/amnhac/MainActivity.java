31c578311fa1023d80a9515d73169e5a5a4983fc
Mr Tai <tainguyenngoc>
2019-11-15 15:03:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/amnhac/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.amnhac;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Fragment_Trang_Chu()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new Fragment_Trang_Chu();
+                    break;
+
+                case R.id.nav_search:
+                    selectedFragment = new Fragment_Tim_Kiem();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+            return true;
+        }
+    };
+}
